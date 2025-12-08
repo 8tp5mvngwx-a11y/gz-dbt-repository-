525dc5cb5dc4f8bff89a027b478dedbdd8081ff6
selen.aksit@gmail.com <selen.aksit@gmail.com>
2025-12-08 08:51:00 +0000

    int_sales_margin

--- models/intermediate/int_sales_margin.sql
+++ models/intermediate/int_sales_margin.sql
@@ -1,6 +1,6 @@
 SELECT *, 
-        (sales.quantity * product.purchase_price) as purchase_cost,
-        (sales.revenue - (sales.quantity * product.purchase_price) ) as margin
+        ROUND ((sales.quantity * product.purchase_price),2) as purchase_cost,
+        ROUND ((sales.revenue - (sales.quantity * product.purchase_price) ),2) as margin
 FROM {{ ref('stg_raw__sales')}} AS sales
 LEFT JOIN {{ ref('stg_raw__product')}} AS product
 ON sales.products_id = product.products_id
